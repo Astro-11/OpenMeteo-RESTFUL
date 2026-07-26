e4ba12851d8a3be2634c715a039fdb5b45b60f41
Astro-11 <astro11.and@gmail.com>
2026-07-26 19:01:58 +0200

    Added further checks in WeatherController

--- src/main/java/com/andrea/weather/controller/WeatherController.java
+++ src/main/java/com/andrea/weather/controller/WeatherController.java
@@ -7,14 +7,16 @@ import com.andrea.weather.service.StatisticsService;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/weather")
 public class WeatherController {
 
-    private final StatisticsService statisticsService;
-    private final CityRepository cityRepository;
+	private final StatisticsService statisticsService;
+	private final CityRepository cityRepository;
 
 	public WeatherController(StatisticsService statisticsService, CityRepository cityRepository) {
 
@@ -25,17 +27,21 @@ public class WeatherController {
 	@GetMapping("/averages")
 	public List<WeatherAverageResponse> getAllAverages() {
 
-		return cityRepository.findAll().stream()
-				.map(city -> statisticsService.getAverage(city.getName())).toList();
+		return cityRepository.findAll()
+				.stream().map(city -> statisticsService.getAverage(city.getName())).toList();
 	}
 
-    @GetMapping("/averages/{city}")
+	@GetMapping("/averages/{city}")
 	public WeatherAverageResponse getAverage(@PathVariable String city) {
-		return statisticsService.getAverage(city);
-    }
+		try {
+			return statisticsService.getAverage(city);
+		} catch (RuntimeException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+		}
+	}
 
-    @GetMapping("/cities")
-    public List<City> getCities() {
-        return cityRepository.findAll();
-    }
+	@GetMapping("/cities")
+	public List<City> getCities() {
+		return cityRepository.findAll();
+	}
 }
